2d27ad838dc97b1ab3b00a976c22e1cbc2b4989f
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-04-14 00:20:02 -0400

    cat breed search

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+
+const request = require('request');
+
+const breed = process.argv.slice(2);
+console.log(breed);
+request('https://api.thecatapi.com/v1/breeds/search?q='+breed, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the dog breed
+  console.log(typeof body);
+  const data = JSON.parse(body);
+  //console.log(data);
+ // console.log(typeof data);
+ try{
+  console.log(data[0].name);
+ } catch(error){
+  console.log(error.message);
+ }
+   
+
+});
