3f95bb74120a056826c117cd2ea55fdc4c591f3d
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-04-16 08:57:54 -0400

    Testing breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,18 @@
 const request = require('request');
 const breed = process.argv.slice(2);
 console.log(breed);
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the dog breed
-  console.log(typeof body);
-  const data = JSON.parse(body);
-  try {
-    console.log(data[0].name);
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) { 
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log(typeof body);
+    const data = JSON.parse(body);
+    try {
+      callback(null, data[0].description);
+    } catch (error) {
+      callback(error, "Invalid Breed");
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
